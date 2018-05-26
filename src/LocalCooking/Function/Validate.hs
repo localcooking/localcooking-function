fecf3010120c4f9776d042190936da44377614a7
Athan Clark <athan.clark@localcooking.com>
2018-05-26 17:48:01 -0600

    more validation

--- src/LocalCooking/Function/Validate.hs
+++ src/LocalCooking/Function/Validate.hs
@@ -5,8 +5,12 @@
 module LocalCooking.Function.Validate where
 
 import LocalCooking.Function.System (AppM, SystemEnv (..))
-import LocalCooking.Database.Schema.User (Unique (UniqueEmail))
+import LocalCooking.Database.Query.Semantics (getChefId, getMenuId, getMealId)
+import LocalCooking.Database.Schema.User
+  (Unique (UniqueEmail))
 
+import Data.Time.Calendar (Day)
+import Data.Text.Permalink (Permalink)
 import Text.EmailAddress (EmailAddress)
 import Control.Monad.Reader (ask)
 import Control.Monad.IO.Class (MonadIO (liftIO))
@@ -24,3 +28,33 @@ uniqueEmail e = do
     case mEnt of
       Nothing -> pure True
       Just _ -> pure False
+
+
+uniqueChefPermalink :: Permalink -> AppM Bool
+uniqueChefPermalink p = do
+  SystemEnv{systemEnvDatabase} <- ask
+
+  mId <- liftIO (getChefId systemEnvDatabase p)
+  pure $ case mId of
+    Nothing -> False
+    Just _ -> True
+
+
+uniqueMenuDeadline :: Permalink -> Day -> AppM Bool
+uniqueMenuDeadline p d = do
+  SystemEnv{systemEnvDatabase} <- ask
+
+  mId <- liftIO (getMenuId systemEnvDatabase p d)
+  pure $ case mId of
+    Nothing -> False
+    Just _ -> True
+
+
+uniqueMealPermalink :: Permalink -> Day -> Permalink -> AppM Bool
+uniqueMealPermalink c d m = do
+  SystemEnv{systemEnvDatabase} <- ask
+
+  mId <- liftIO (getMealId systemEnvDatabase c d m)
+  pure $ case mId of
+    Nothing -> False
+    Just _ -> True
